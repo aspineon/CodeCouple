c05264e09e0eb45f9c61e12ce022ab3ac50c49fe
Krzysztof Chrusciel <krzysztof.chrusciel@outlook.com>
2017-01-22 16:21:36 +0000

    Add SolrAutocomplete

---
+++ SolrAutocomplete/src/main/java/SolrAutocomplete.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Krzysztof Chruściel.
+ */
+@SpringBootApplication
+public class SolrAutocomplete {
+    public static void main(String[] args) {
+        SpringApplication.run(SolrAutocomplete.class, args);
+    }
+}
