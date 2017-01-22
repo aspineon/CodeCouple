7a4a387a28182fc103f8f101db890df89a515694
Krzysztof Chrusciel <krzysztof.chrusciel@outlook.com>
2017-01-22 17:37:49 +0000

    Remove classes

--- SolrAutocomplete/target/classes/static/js/script.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $("#product_name")
-        .autocomplete(
-            {
-                source : 'http://localhost:8083/autocomplete',
-                minLength : 1,
-            });
-});
